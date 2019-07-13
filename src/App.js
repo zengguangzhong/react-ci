d4636825e9b4f4cca3e8e2d749be9c9dbad320c3
zengguangzhong <zengguangzhong1989@gmail.com>
2019-07-13 16:02:25 +0800

    'test1'

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 1190
+          Learn React 3
         </a>
       </header>
     </div>
